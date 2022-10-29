b31de496da8a49ec70f7470c43391f23dd84a89a
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-10-29 21:55:59 +0530

    Update and rename co-47.c  || operator to co-47  || operator.c

--- co-47  || operator.c
+++ co-47  || operator.c
@@ -3,6 +3,7 @@ int main()
 {
   int x = 5;
   int y = 3;
+  printf("RA2211042010046");
   // Returns 1 (true) because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
   printf("%d", x > 3 || x < 4);
   return 0;
