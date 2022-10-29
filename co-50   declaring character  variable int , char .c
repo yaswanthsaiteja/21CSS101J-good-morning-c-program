bcfdd311815036dc633ba03cf02f48f009974044
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-10-29 22:20:34 +0530

    Update and rename co-50 .c  declaring character  variable int , char to co-50   declaring character  variable int , char .c

--- co-50   declaring character  variable int , char .c
+++ co-50   declaring character  variable int , char .c
@@ -2,7 +2,8 @@
 int main() 
 {
   int myNum = 72820;
-  char myname = 'sai';
+  char myname = 'sai'; 
+  printf("RA2211042010046\n");
   printf("My number is %d\n and my letter is %c\n", myNum, myname);
   return 0;
 }
