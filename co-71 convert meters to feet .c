1857729ac59255e740427a786317692815a09513
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-02 23:15:47 +0530

    Update co-71 convert meters to feet .c

--- co-71 convert meters to feet .c
+++ co-71 convert meters to feet .c
@@ -3,8 +3,9 @@ void main() {
   float meter;
   float feet;
     clrscr();
-        printf(" Program to convert Meter to Feet:  ");
-        printf("\n\n Enter Length in Meters  : ");
+	printf("RA2211042010046\n");
+        printf(" Program to convert Meter to Feet:");
+        printf("\n\n Enter Length in Meters: ");
 	scanf("%f", &meter);
          feet = (meter*3.26);
          printf("\n\n %f meter in feet = %f  \n",meter,feet);
