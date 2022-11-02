f0e8c691a24544088c44c771b9e1209f19afd109
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-02 22:58:16 +0530

    Update and rename co-70.c  convert degree to fahrenheit to co-70  convert degree to fahrenheit .c

---
+++ co-70  convert degree to fahrenheit .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+float degree,fahren 
+
+	printf(" Program to convert Temperature Degree to Fahrenheit :  ");
+        printf("\n\n Enter Temperature in Degrees  : ");
+	scanf("%f", °ree);
+        fahrenheit = (1.8*degree)+32;
+        printf("\n\n %f Degree in Fahrenheit = :  %f  \n",degree,fahrenheit);
+//	getch();
+}
