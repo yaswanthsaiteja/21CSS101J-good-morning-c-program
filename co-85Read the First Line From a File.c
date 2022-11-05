49c4df59dafd2ae963105a95bd185060257b64cb
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-05 12:26:58 +0530

    Update and rename co-85.c Read the First Line From a File to co-85Read the First Line From a File.c

--- co-85Read the First Line From a File.c
+++ co-85Read the First Line From a File.c
@@ -5,12 +5,8 @@ int main() {
     FILE *fptr;
     if ((fptr = fopen("program.txt", "r")) == NULL) {
         printf("Error! File cannot be opened.");
-        // Program exits if the file pointer returns NULL.
         exit(1);
-    }
-
-    // reads text until newline is encountered
-    fscanf(fptr, "%[^\n]", c);
+    } fscanf(fptr, "%[^\n]", c);
     printf("Data from the file:\n%s", c);
     fclose(fptr);
 
