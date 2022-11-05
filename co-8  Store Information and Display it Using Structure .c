6c5565521f6d89bf3122cc5f89319973314341a6
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-05 12:01:22 +0530

    Update and rename co-81.c  Store Information and Display it Using Structure to co-8  Store Information and Display it Using Structure .c

--- co-8  Store Information and Display it Using Structure .c
+++ co-8  Store Information and Display it Using Structure .c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-struct student {
-    char name[50];
+int main();{
+   char name[50];
     int roll;
     float marks;
-} s;
 
-int main() {
     printf("Enter information:\n");
     printf("Enter name: ");
     fgets(s.name, sizeof(s.name), stdin);
