2b795becce8668369c4f0f0894feee77987bf0d3
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-08 08:35:21 +0530

    Update and rename co-94.c  miles to kilometers to co-94 miles to kilometers .c

--- co-94 miles to kilometers .c
+++ co-94 miles to kilometers .c
@@ -2,14 +2,10 @@
 int main()
 {
  float miles, kilometers;
-
+  printf("RA2211042010046\n");
   printf("Enter distance in Miles=");
   scanf("%f", &miles);
-
   kilometers = miles * 1.609;
-
   printf("%.2f Miles are equal to %.2f kilometers", miles,kilometers);
-
   return 0;
-
-}
+  }
