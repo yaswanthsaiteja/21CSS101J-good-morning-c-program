17fffa77f30faaf265e65ad0c69b562aaee1e730
yaswanthsaiteja <113873651+yaswanthsaiteja@users.noreply.github.com>
2022-11-20 23:38:29 +0530

    Create address of num1&num2 co-103.c

---
+++ address of num1&num2 co-103.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+int  num1;
+   char num2[10];
+   printf("Address of num1 variable: %x\n", &num1  );
+   printf("Address of num2 variable: %x\n", &num2  );
+   return 0;
+}
